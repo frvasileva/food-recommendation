8c19f7d89ec3b0d3219c7eb8f0b4e8098114bc8c
frvasileva <fanka@klearlending.com>
2020-02-22 22:52:35 +0200

    IngredientList toggling added

--- ui/src/components/IngredientsList/IngredientsList.tsx
+++ ui/src/components/IngredientsList/IngredientsList.tsx
@@ -2,10 +2,27 @@ import React from "react";
 import "./IngredientsList.scss";
 
 export const IngredientsList = (props: any) => {
+  var ingredientsFromProps = props.ingredients.reduce((acc: any, item: any) => {
+    acc[item] = false;
+    return acc;
+  }, {});
+
+  const [ingredients, setIngredients] = React.useState(ingredientsFromProps);
+
+  function selectProduct(ingredient: any) {
+    ingredients[ingredient] = !ingredients[ingredient];
+    var newIngredients = { ...ingredients };
+    setIngredients(newIngredients);
+  }
+
   return (
     <ul className="ingredients-list">
-      {props.ingredients.map((ingredient: string) => (
-        <li key={ingredient} className="item">
+      {Object.keys(ingredients).map((ingredient: string) => (
+        <li
+          key={ingredient}
+          className={`item ${ingredients[ingredient] ? "selected" : ""}`}
+          onClick={() => selectProduct(ingredient)}
+        >
           <div className="row">
             <div className="col-md-5">
               <i className="fas fa-check-square"></i> {ingredient}
